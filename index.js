6257dc303cfed0589cebeedb144b07590b5e390b
Shumbusho43 <davidshumbusho10@gmail.com>
2024-06-01 18:34:34 +0200

    added deletete,update and

--- index.js
+++ index.js
@@ -31,19 +31,51 @@ app.get('/citizens', (req, res) => {
 
 Homework add these endpoints
 
-- GET    /citizens/:id - get a single citizen by id
-- POST   /citizens - create a new citizen
+- GET    /citizens/:id - get a single citizen by id===done
+- POST   /citizens - create a new citizen  ====done
 
-- PUT    /citizens/:id - update a citizen
+- PUT    /citizens/:id - update a citizen==done
 - DELETE /citizens/:id - delete a citizen
 - PATCH  ???
 
 */
+//get single citizen by id
+app.get('/citizens/:id', (req, res) => {
+    let id = req.params.id;
+    connection.query(`SELECT * from citizens where id=${id}`, function (error, results, fields) {
+        if (error) throw error;
+        res.send({
+            citizen: results
+        })
+    });
+});
+//update a citizen
+app.put('/citizens/:id', (req, res) => {
+    let id = req.params.id;
+    let name = req.body.name;
+    let data_of_birth = req.body.date_of_birth;
+    connection.query(`UPDATE citizens SET name='${name}',date_of_birth='${data_of_birth}' WHERE id=${id}`, function (error, results, fields) {
+        if (error) throw error;
+        res.send({
+            message: "Citizen updated successfully"
+        })
 
+    });
+})
+//delete a citizen by id
+app.delete('/citizens/:id', (req, res) => {
+    let id = req.params.id;
+    connection.query(`DELETE FROM citizens WHERE id=${id}`, function (error, results, fields) {
+        if (error) throw error;
+        res.send({
+            message: "Citizen deleted successfully"
+        })
+    });
+});
 //insert a new citizen
 app.post('/citizens', (req, res) => {
-    let name=req.body.name;
-    let data_of_birth =req.body.date_of_birth;
+    let name = req.body.name;
+    let data_of_birth = req.body.date_of_birth;
     let gender = req.body.gender
     let status = req.body.status
     connection.query(`INSERT INTO citizens (name,date_of_birth,gender,status) VALUES ('${name}','${data_of_birth}','${gender}','${status}')`, function (error, results, fields) {
